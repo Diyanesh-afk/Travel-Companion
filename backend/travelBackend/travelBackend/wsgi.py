d0cb6007defc701aaf2613b1e67d7da5daa7f2c7
Diyanesh-afk <naagadiyaneshn@gmail.com>
2026-09-25 15:12:52 +0530

    Fix Vercel Django WSGI import

--- backend/travelBackend/travelBackend/wsgi.py
+++ backend/travelBackend/travelBackend/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for travelBackend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# Make the directory containing manage.py available to Python.
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "travelBackend.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'travelBackend.settings')
 
 application = get_wsgi_application()
